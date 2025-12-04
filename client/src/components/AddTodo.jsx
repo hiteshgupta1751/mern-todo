d073327a2c67cde98429d97b33f624111a20b90d
root <root@ip-172-31-13-172.eu-north-1.compute.internal>
2025-12-04 08:41:08 +0000

    Initial commit - project

---
+++ client/src/components/AddTodo.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const API_BASE = "http://13.53.45.43:5000/api/todos";
+
+function AddTodo({ todos, setTodos }) {
+    const [editMode, setEditMode] = useState(false);
+    const [editId, setEditId] = useState(null);
+    const [updatedTitle, setUpdatedTitle] = useState('');
+    const [updatedDescription, setUpdatedDescription] = useState('');
+
+    if (!Array.isArray(todos)) {
+        return null;
+    }
+
+    const handleDel = (id) => {
+        fetch(`${API_BASE}/${id}`, {
+            method: 'DELETE'
+        }).then((resp) => resp.json())
+          .then((data) => {
+              console.log("Deleted:", data);
+              setTodos(data);
+          });
+    }
+
+    const handleEdit = (id) => {
+        fetch(`${API_BASE}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: updatedTitle,
+                description: updatedDescription
+            })
+        }).then((resp) => resp.json())
+          .then((data) => {
+              console.log("Updated:", data);
+              setTodos(data);
+              setEditMode(false);
+              setEditId(null);
+          });
+    }
+
+    return (
+        <>
+            {todos.map((todo) => (
+                <div key={todo.id} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: 20 }}>
+                    <Paper style={{ width: '30%', textAlign: 'center', marginLeft: '6%', marginRight: 10 }}>
+                        {editMode && editId === todo.id ? (
+                            <>
+                                <TextField
+                                    label="Title"
+                                    value={updatedTitle}
+                                    onChange={e => setUpdatedTitle(e.target.value)}
+                                />
+                                <TextField
+                                    label="Description"
+                                    value={updatedDescription}
+                                    onChange={e => setUpdatedDescription(e.target.value)}
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <div style={{ margin: 8 }}><h2>{todo.title}</h2></div>
+                                <div style={{ margin: 8 }}>{todo.description}</div>
+                            </>
+                        )}
+                    </Paper>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <Button onClick={() => handleDel(todo.id)} variant="contained" color="error">Delete</Button>
+                        {editMode && editId === todo.id ? (
+                            <Button onClick={() => handleEdit(todo.id)} variant="contained" color="success">Save</Button>
+                        ) : (
+                            <Button
+                                onClick={() => {
+                                    setEditMode(true);
+                                    setEditId(todo.id);
+                                    setUpdatedTitle(todo.title);
+                                    setUpdatedDescription(todo.description);
+                                }}
+                                variant="contained"
+                                color="success"
+                            >
+                                Edit
+                            </Button>
+                        )}
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default AddTodo;
